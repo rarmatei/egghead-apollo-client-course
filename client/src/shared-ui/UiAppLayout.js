07bdfd17710b73341e38fafc15f55973ae4e8476
Rares Matei <matei.rar@gmail.com>
2021-08-15 11:35:56 +0100

    app layout accept single child

--- client/src/shared-ui/UiAppLayout.js
+++ client/src/shared-ui/UiAppLayout.js
@@ -2,15 +2,27 @@ import { UiAppMainHeader } from "./UiAppMainHeader";
 import { Box, Flex } from "@chakra-ui/react";
 
 export function UiAppLayout({ children }) {
-  return (
-    <>
-      <UiAppMainHeader />
+  let contents;
+  if (children && Array.isArray(children) && children.length > 0) {
+    contents = (
       <Flex padding="10px" alignItems="flex-start">
         <Box>{children && children[0]}</Box>
         <Box paddingLeft="30px" width="450px">
           {children && children[1]}
         </Box>
       </Flex>
+    );
+  } else {
+    contents = (
+      <Flex>
+        <Box>{children}</Box>
+      </Flex>
+    );
+  }
+  return (
+    <>
+      <UiAppMainHeader />
+      {contents}
     </>
   );
 }
