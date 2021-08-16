481da4932e27dc18a6d4c3a8fd798d46192c8c14
Rares Matei <matei.rar@gmail.com>
2021-08-16 20:03:33 +0100

    export all shared components toplevel

---
+++ client/src/shared-ui/index.js
@@ -0,0 +1,12 @@
+export * from './UiNote';
+export * from './DeleteButton';
+export * from './UiAppLayout';
+export * from './UiAppMainHeader';
+export * from './UiCollapse';
+export * from './UiEditCategories';
+export * from './UiEditCategoriesButton';
+export * from './UiEditNote';
+export * from './UiLoadMoreButton';
+export * from './UiNotFound';
+export * from './ViewButton';
+
