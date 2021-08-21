e901a20020a91e2289b39ed372d038b058280a8d
Rares Matei <matei.rar@gmail.com>
2021-08-21 12:59:56 +0100

    finish lesson 7

--- client/src/EditNote.js
+++ client/src/EditNote.js
@@ -1,7 +1,39 @@
+import { gql, useQuery, useMutation } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { UiEditNote } from "./shared-ui";
+
+const GET_NOTE = gql`
+  query GetNote($id: String!) {
+    note(id: $id) {
+      id
+      content
+    }
+  }
+`;
 
 export function EditNote() {
   let { noteId } = useParams();
 
-  return <div>Note with id {noteId} selected.</div>;
+  const { data } = useQuery(GET_NOTE, {
+    variables: {
+      id: noteId
+    }
+  });
+
+  const [updateNote, {loading}] = useMutation(gql`
+    mutation UpdateNote($id: String!, $content: String!) {
+      updateNote(id: $id, content: $content) {
+        successful
+      }
+    }
+  `)
+
+  return <UiEditNote isSaving={loading} onSave={(newContent) => {
+    updateNote({
+      variables: {
+        id: noteId,
+        content: newContent
+      }
+    })
+  }} note={data?.note} />;
 }
