c45d6c05226d74cece053b7736f0009416158383
Rares Matei <matei.rar@gmail.com>
2021-08-21 09:03:41 +0100

    finish lesson 6

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
+import { RetryLink } from '@apollo/client/link/retry';
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 2000,
+    max: 2000,
+    jitter: false
+  }
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
+  link: from([retryLink, httpLink])
 });
 
 ReactDOM.render(
