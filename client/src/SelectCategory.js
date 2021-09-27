204db15f89444232c440c3e54d1af2aed53cf49b
Rares Matei <matei.rar@gmail.com>
2021-09-27 10:29:54 +0100

    finish lesson 21

--- client/src/SelectCategory.js
+++ client/src/SelectCategory.js
@@ -3,7 +3,9 @@ import { useQuery } from "@apollo/client";
 import { ALL_CATEGORIES_QUERY } from "./EditCategories";
 
 function SelectCategory({ onCategoryChange, defaultValue }) {
-  const { data } = useQuery(ALL_CATEGORIES_QUERY);
+  const { data } = useQuery(ALL_CATEGORIES_QUERY, {
+    pollInterval: 1000
+  });
   return (
     <Box>
       <Select
